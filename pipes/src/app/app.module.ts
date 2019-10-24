057e567bb9a7cf648fe1c20d8977b888e3d9157e
jespinoza <juan.espinoza@fluxit.com.ar>
2019-10-24 15:11:44 -0300

    Código para cambiar el formato de ingles a español en la fecha

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
