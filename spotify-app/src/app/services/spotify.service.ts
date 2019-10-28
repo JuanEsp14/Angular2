990ac6d93341014af248e5d3d182380d46c1f5fa
jespinoza <juan.espinoza@fluxit.com.ar>
2019-10-28 11:46:03 -0300

    Se crea el buscador

--- spotify-app/src/app/services/spotify.service.ts
+++ spotify-app/src/app/services/spotify.service.ts
@@ -16,4 +16,12 @@ export class SpotifyService {
     )
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
   }
+
+  getArtist(termino:string){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBCyVbnk3R8S5CH0rJL2su-jxlTTZLFHjS_wm46_xdiDz7v-kXCaRUKkWnu5LCX7qRuTfuGv-LEkK4R0Gs'
+      }
+    )
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, {headers});
+  }
 }
