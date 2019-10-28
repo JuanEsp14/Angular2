cd507a82c79bbbe7fd2dd3f0e67087f3182e4330
jespinoza <juan.espinoza@fluxit.com.ar>
2019-10-28 14:52:10 -0300

    Se agregan los errores

--- spotify-app/src/app/components/home/home.component.ts
+++ spotify-app/src/app/components/home/home.component.ts
@@ -10,14 +10,21 @@ export class HomeComponent implements OnInit {
 
   nuevasCanciones:any[] = [];
   loading:boolean;
+  error:boolean;
+  messageError:string;
 
   constructor( private spotify:SpotifyService ) { 
 
     this.loading = true;
+    this.error = false;
     this.spotify.getNewRealeses()
       .subscribe( (data:any) => {
         this.nuevasCanciones = data;
         this.loading = false;
+      },(errorServicio) => {
+        this.loading = false;
+        this.messageError = errorServicio.error.error.message;
+        this.error = true;
       });    
   }
 
