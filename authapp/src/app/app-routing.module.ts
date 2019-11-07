0c3bd9af6ac0499205e1b750c69da76aa61231be
jespinoza <juan.espinoza@fluxit.com.ar>
2019-11-07 14:50:30 -0300

    Se agregan los componentes y navegación

---
+++ authapp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { PrecioComponent } from './components/precio/precio.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+
+const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'precios', component: PrecioComponent},
+    {path:'protegida', component: ProtegidaComponent},
+    {path:'**', pathMatch:'full', redirectTo:'home'},
+];
+
+@NgModule
+    (
+    {
+        exports: [RouterModule],
+        imports: [RouterModule.forRoot(routes)],
+    }
+    )
+export class AppRoutingModule {}
--- authapp/src/app/app.module.ts
+++ authapp/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PrecioComponent } from './components/precio/precio.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PrecioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
