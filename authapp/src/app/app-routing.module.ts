cb7ea04a924d7f564c1c44498cc06b45fc559e42
jespinoza <juan.espinoza@fluxit.com.ar>
2019-11-07 16:12:20 -0300

    Se agrega el funcionamiento del Guard para proteger la página protegida

--- authapp/src/app/app-routing.module.ts
+++ authapp/src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from './services/auth.guard';
+
 import { HomeComponent } from './components/home/home.component';
 import { PrecioComponent } from './components/precio/precio.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 
+
 const routes: Routes = [
     {path:'home', component: HomeComponent},
     {path:'precios', component: PrecioComponent},
-    {path:'protegida', component: ProtegidaComponent},
+    {
+        path:'protegida', 
+        component: ProtegidaComponent,
+        canActivate: [ AuthGuard ]
+    },
     {path:'**', pathMatch:'full', redirectTo:'home'},
 ];
 
