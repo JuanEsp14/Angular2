1b757c3b796af76e3bfbad8145c2048db0c843d2
jespinoza <juan.espinoza@fluxit.com.ar>
2019-11-08 16:35:53 -0300

    Se agrega SweetAlert2 en el login

--- login-app-propio/src/app/pages/login/login.component.ts
+++ login-app-propio/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -20,12 +21,27 @@ export class LoginComponent implements OnInit {
 
   login( form:NgForm ){
     if(form.invalid) return;
+    
+    Swal.fire({
+      //Persona no puede salir haciendo click afuera
+      allowOutsideClick:false,
+      text: 'Espere por favor...',
+      icon:'info',
+    });
+
+    Swal.showLoading();
 
     this.auth.login(this.usuario)
       .subscribe(resp =>{
         console.log(resp);
+        Swal.close();
       }, (err) => {
         console.log(err.error.error.message);
+        Swal.fire({
+          title: 'Error al iniciar sesión',
+          text: err.error.error.message,
+          icon:'error',
+        });
       });
   }
 
