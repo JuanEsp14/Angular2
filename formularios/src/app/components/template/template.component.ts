04e03626b882de3fbadd9a51c6015f11c12a6dcc
jespinoza <juan.espinoza@fluxit.com.ar>
2019-11-12 12:42:11 -0300

    Se agregan validaciones de ngModel al formulario

--- formularios/src/app/components/template/template.component.ts
+++ formularios/src/app/components/template/template.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario:Object = {
+    nombre: "Juan",
+    apellido: "Espinoza",
+    correo: ""
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardar(forma:NgForm){
+    console.log("Formulario posteado");
+    console.log("ngForm ",forma);
+    console.log("valor", forma.value);
+
+    console.log("usuario", this.usuario);
   }
 
 }
