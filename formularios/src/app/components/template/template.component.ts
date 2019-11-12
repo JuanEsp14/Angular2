0bb6996b683d432c1deb8d03818c16ea5d5e1e1d
jespinoza <juan.espinoza@fluxit.com.ar>
2019-11-12 12:50:46 -0300

    Se agrega css para la validación de los campos

--- formularios/src/app/components/template/template.component.ts
+++ formularios/src/app/components/template/template.component.ts
@@ -5,14 +5,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form){
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
 
   usuario:Object = {
-    nombre: "Juan",
-    apellido: "Espinoza",
-    correo: ""
+    nombre: null,
+    apellido: null,
+    correo: null
   }
 
   constructor() { }
