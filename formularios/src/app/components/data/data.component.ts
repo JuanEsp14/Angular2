94693e3c52881658635edab539b1919c7b23a455
jespinoza <juan.espinoza@fluxit.com.ar>
2019-11-12 16:56:00 -0300

    Se cambia la estructura del formulario

--- formularios/src/app/components/data/data.component.ts
+++ formularios/src/app/components/data/data.component.ts
@@ -11,17 +11,28 @@ export class DataComponent  {
 
   forma:FormGroup;
 
+  usuario:Object = {
+    nombreCompleto:{
+      nombre: "juan",
+      apellido: "Espinoza"
+    },
+    correo: "juan.espinoza@corre.com"
+  }
+
   constructor() { 
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [
-                                      Validators.required,
-                                      Validators.minLength(3)
-                                    ]),
-      'apellido': new FormControl('', Validators.required),
+
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        'apellido': new FormControl('', Validators.required)
+      }),
       'correo':  new FormControl('', [Validators.required, 
-                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-                                )])
-    });
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                                    ])
+    }); 
   }
 
   guardarCambios(){
