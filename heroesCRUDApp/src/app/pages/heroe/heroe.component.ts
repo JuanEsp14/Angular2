ecb3364bba2645d9e0330dd52dfd97f26c270dee
jespinoza <juan.espinoza@fluxit.com.ar>
2020-02-01 16:35:16 -0300

    Se crea el servicio para recuperar un heroe especifico para editarlo

--- heroesCRUDApp/src/app/pages/heroe/heroe.component.ts
+++ heroesCRUDApp/src/app/pages/heroe/heroe.component.ts
@@ -5,6 +5,7 @@ import { HeroesService } from '../../services/heroes.service';
 
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 
 
@@ -17,9 +18,20 @@ export class HeroeComponent implements OnInit {
 
   heroe:HeroeModel = new HeroeModel();
 
-  constructor(private heroesService:HeroesService) { }
+  constructor(private heroesService:HeroesService, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    
+    const id = this.route.snapshot.paramMap.get('id');
+    
+    if(id !== 'nuevo'){
+      this.heroesService.getHeroe(id)
+        .subscribe((resp: HeroeModel) => {
+          this.heroe = resp;
+          this.heroe.id = id;
+        });
+    }
+
   }
   guardar(form: NgForm){
 
