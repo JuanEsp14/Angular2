570e2196c291c11ed31644d84e73fe698347d4fb
jespinoza <juan.espinoza@fluxit.com.ar>
2020-02-04 16:25:49 -0300

    Se agregan los nuevos servicios para la app

--- peliculas-app/src/app/services/peliculas.service.ts
+++ peliculas-app/src/app/services/peliculas.service.ts
@@ -19,6 +19,39 @@ export class PeliculasService {
                     .pipe(map( (res:any)=>res.results));
   }
 
+  getCartelera(){
+    let desde = new Date();
+    let hasta = new Date();
+    hasta.setDate(desde.getDate()+7);
+
+    let desdeStr = this._armarFecha(desde);
+    let hastaStr = this._armarFecha(hasta);
+
+    let url = `${ this.urlMoviedb}/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&api_key=${this.apikey}&language=es`;
+
+    return this.http.get( url )
+                    .pipe(map( (res:any)=>res.results));
+  }
+
+  private _armarFecha(date: Date){
+    let fecha = "";
+    let mes = "";
+    let dia = "";
+    (date.getMonth()+1 < 10) ? mes = `0${date.getMonth()+1}` : mes = `${date.getMonth()+1}`;
+    (date.getDate() < 10) ? dia = `0${date.getDate()}` : dia = `${date.getDate()}`;
+    
+    fecha = `${date.getFullYear()}-${mes}-${dia}`;
+
+    return fecha;
+  }
+
+  getPopularesNinos(){
+    let url = `${ this.urlMoviedb}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=${this.apikey}&language=es`;
+
+    return this.http.get( url )
+                    .pipe(map( (res:any)=>res.results));
+  }
+
   buscarPelicula(texto:string){
     let url = `${ this.urlMoviedb}/search/movie?query=${texto}&sort_by=popularity.desc&api_key=${this.apikey}&language=es`;
 
