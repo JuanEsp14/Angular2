bdc5306b7908df66a08255890cbe4c4603f3db2b
jespinoza <juan.espinoza@fluxit.com.ar>
2020-02-07 16:33:31 -0300

    Fix buscar

--- peliculas-app/src/app/app.routes.ts
+++ peliculas-app/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { PeliculaComponent } from './components/pelicula/pelicula.component';
 
 const app_routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'buscar', component: BuscarComponent },
     { path: 'buscar/:texto', component: BuscarComponent },
     { path: 'pelicula/:id/:pag', component: PeliculaComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
