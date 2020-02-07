2546a83a21d402cc725905ad132af8043b766892
jespinoza <juan.espinoza@fluxit.com.ar>
2020-02-07 16:31:31 -0300

    REtoques y finalización de la aplicación de peliculas

--- peliculas-app/src/app/components/pelicula/pelicula.component.ts
+++ peliculas-app/src/app/components/pelicula/pelicula.component.ts
@@ -10,9 +10,11 @@ import { ActivatedRoute } from '@angular/router';
 export class PeliculaComponent implements OnInit {
 
   pelicula:any;
+  pag:string = "";
 
   constructor(public _ps:PeliculasService, public router:ActivatedRoute) { 
     this.router.params.subscribe(parametros =>{
+      this.pag = parametros['pag'];
       this._ps.getPelicula(parametros['id'])
         .subscribe(pelicula => this.pelicula = pelicula)
     });
