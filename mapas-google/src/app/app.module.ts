c1a7a46352e0584d705d7eafecd998e93dbc5078
jespinoza <juan.espinoza@fluxit.com.ar>
2020-02-10 16:44:16 -0300

    Se agrega el windows info en el marcador

--- mapas-google/src/app/app.module.ts
+++ mapas-google/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { MapaComponent } from './components/mapa/mapa.component';
+import { AgmCoreModule } from '@agm/core'
+import { asapScheduler } from 'rxjs';
 
 
 
@@ -16,7 +18,10 @@ import { MapaComponent } from './components/mapa/mapa.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCogXeH-yxi44MP_abLxDrGLBHJ-Syvnw4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
