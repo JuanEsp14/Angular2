d18eeecd9ac1374a70e0cad3df7fb272884fc0fb
jespinoza <juan.espinoza@fluxit.com.ar>
2020-02-11 20:25:41 -0300

    Se agrega la clase de marcador y se agregan en el mapa a traves del evento mapClick

--- mapas-google/src/app/components/mapa/mapa.component.ts
+++ mapas-google/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../clases/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -9,10 +10,19 @@ export class MapaComponent implements OnInit {
 
   lat:number = 51.678418;
   lng:number = 7.809007;
+  marcadores: Marcador[] = [];
 
-  constructor() { }
+  constructor() { 
+    const nuevoMarcador = new Marcador(this.lat, this.lng);
+    this.marcadores.push(nuevoMarcador);
+  }
 
   ngOnInit() {
+  }
+
+  agregarMarcador(evento){
+    const nuevoMarcador = new Marcador(evento.coords.lat, evento.coords.lng);
+    this.marcadores.push(nuevoMarcador);
   }
 
 }
