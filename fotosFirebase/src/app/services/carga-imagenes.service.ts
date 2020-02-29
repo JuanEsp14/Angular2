53acfb07561b99fed25a48ae1d68a63223d4e245
jespinoza <juan.espinoza@fluxit.com.ar>
2020-02-29 18:30:33 -0300

    Se guardan las imagenes en FireBase de manera correcta

--- fotosFirebase/src/app/services/carga-imagenes.service.ts
+++ fotosFirebase/src/app/services/carga-imagenes.service.ts
@@ -18,10 +18,40 @@ export class CargaImagenesService {
   constructor(private db:AngularFirestore) { }
 
   cargarImagenesFirebase( imagenes: FileItem[] ){
-    console.log( imagenes );
+    const storageRef =  firebase.storage().ref();
+
+    for(const item of imagenes){
+      item.estaSubiendo = true;
+      if(item.progreso >= 100){
+        continue;
+      }
+
+      const uploadTask: firebase.storage.UploadTask = storageRef.child
+                (`${ this.CARPETA_IMAGENES}/${ item.nombreArchivo}`)
+                  .put( item.archivo );
+      
+      uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
+         ( snapshot ) => item.progreso = ( snapshot.bytesTransferred/snapshot.totalBytes ) * 100,
+         ( error ) => console.error('Error al subir', error),
+         ( ) => {
+            uploadTask.snapshot.ref.getDownloadURL()
+            .then((url) => {
+              item.url = url;
+              item.estaSubiendo = false;
+              this.guardarImagen({
+                nombre: item.nombreArchivo,
+                url: item.url
+            });
+          });
+           console.log('Imagen cargada correctamente');
+         }
+      );
+        
+    }
   }
 
   private guardarImagen( imagen:{ nombre: string, url:string } ){
+    console.log('Se va a guardar la imagen');
     this.db.collection(`/${ this.CARPETA_IMAGENES }`)
       .add( imagen );
   }
