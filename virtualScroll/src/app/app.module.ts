2ae4734119bf370b91a4a34782f0499c56f5e717
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-02 14:06:29 -0300

    Se hace drag and Drop en una lista ordenada

--- virtualScroll/src/app/app.module.ts
+++ virtualScroll/src/app/app.module.ts
@@ -2,21 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ScrollingModule, ScrollDispatcher  } from '@angular/cdk/scrolling';
 import { DragDropModule  } from '@angular/cdk/drag-drop';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './components/virtual/virtual.component';
 import { DragComponent } from './components/drag/drag.component';
+import { PaisesComponent } from './components/paises/paises.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VirtualComponent,
-    DragComponent
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [ScrollDispatcher],
   bootstrap: [AppComponent]
